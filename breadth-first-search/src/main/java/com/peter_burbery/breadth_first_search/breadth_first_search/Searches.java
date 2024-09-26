a215d8ceb422356865a9ee04bc825ec13b9e0f50
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-09-25 21:01:10 -0400

    levels are now displayed
    instead of
    
    --- Moving to next level ---
    Finished visiting node 1
    Finished visiting node 3
    
    --- Moving to next level ---
    Finished visiting node 2
    Finished visiting node 4
    Finished visiting node 6
    Finished visiting node 7
    
    --- Moving to next level ---
    Finished visiting node 8
    Finished visiting node 9
    Finished visiting node 5
    
    we have
    
    --- Level 1 ---
    Finished visiting node 1
    Finished visiting node 3
    
    --- Level 2 ---
    Finished visiting node 2
    Finished visiting node 4
    Finished visiting node 6
    Finished visiting node 7
    
    --- Level 3 ---
    Finished visiting node 8
    Finished visiting node 9
    Finished visiting node 5
    
    I told ChatGPT
    
    awesome! I think we can submit that.
    
    However, I would like to make some changes. First, let's number the
    levels so instead of next level, we say level 1 (not level 0 because 1
    and 3 are a distance of 1 from 0), then level 2, then level 3.

--- breadth-first-search/src/main/java/com/peter_burbery/breadth_first_search/breadth_first_search/Searches.java
+++ breadth-first-search/src/main/java/com/peter_burbery/breadth_first_search/breadth_first_search/Searches.java
@@ -7,29 +7,32 @@ package com.peter_burbery.breadth_first_search.breadth_first_search;
 /**
  * 
  */
-import java.util.LinkedList;
-import java.util.Queue;
+
+import com.peter_burbery.mu_queue.mu_queue.MUQueue;
 
 public class Searches {
 
 	public static void BFS(Vertex[] nodes, boolean[][] adjacency, int n, int start) {
-		Queue<Vertex> myQueue = new LinkedList<>();
-		myQueue.add(nodes[start]);
+		MUQueue<Vertex> myQueue = new MUQueue<>();
+		myQueue.enqueue(nodes[start]);
 		nodes[start].setVisited(true);
 
 		System.out.println("Starting BFS traversal from node: " + start);
 
 		// Track levels in BFS using a queue separator (null or custom marker)
-		myQueue.add(null); // Add level separator
+		myQueue.enqueue(null); // Add level separator
+		int level = 1; // Start with level 1
+
 		while (!myQueue.isEmpty()) {
-			Vertex current = myQueue.poll();
+			Vertex current = myQueue.dequeue();
 
 			// If we hit a level separator and the queue is not empty, we know we are moving
 			// to the next level
 			if (current == null) {
 				if (!myQueue.isEmpty()) {
-					myQueue.add(null); // Add another level separator
-					System.out.println("\n--- Moving to next level ---");
+					myQueue.enqueue(null); // Add another level separator
+					System.out.println("\n--- Level " + level + " ---");
+					level++;
 				}
 				continue;
 			}
@@ -41,11 +44,10 @@ public class Searches {
 			// Add adjacent unvisited nodes to the queue
 			for (int j = 0; j < n; j++) {
 				if (adjacency[toVisit][j] && !nodes[j].getVisited()) {
-					myQueue.add(nodes[j]);
+					myQueue.enqueue(nodes[j]);
 					nodes[j].setVisited(true);
 				}
 			}
 		}
 	}
 }
-
