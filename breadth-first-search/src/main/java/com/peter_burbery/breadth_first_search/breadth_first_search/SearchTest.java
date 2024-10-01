65d831306a88f83a044263c2cc1ecd7350fb1275
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-10-01 13:32:17 -0400

    I finished DFS.

--- breadth-first-search/src/main/java/com/peter_burbery/breadth_first_search/breadth_first_search/SearchTest.java
+++ breadth-first-search/src/main/java/com/peter_burbery/breadth_first_search/breadth_first_search/SearchTest.java
@@ -8,63 +8,73 @@ package com.peter_burbery.breadth_first_search.breadth_first_search;
  * 
  */
 public class SearchTest {
-	public static void main(String[] args) {
-		// Create the graph using the adjacency matrix defined inside the Graph class
-		int n = 10; // Number of nodes (0 to 9)
 
-		// Define the adjacency matrix for the graph
-		int[][] adjacencyMatrix = new int[n][n];
+    public static void main(String[] args) {
+        // Create the graph using the adjacency matrix defined inside the Graph class
+        int n = 10; // Number of nodes (0 to 9)
 
-		adjacencyMatrix[0][1] = 1;
-		adjacencyMatrix[1][0] = 1;
+        // Define the adjacency matrix for the graph
+        int[][] adjacencyMatrix = new int[n][n];
 
-		adjacencyMatrix[0][3] = 1;
-		adjacencyMatrix[3][0] = 1;
+        adjacencyMatrix[0][1] = 1;
+        adjacencyMatrix[1][0] = 1;
 
-		adjacencyMatrix[1][2] = 1;
-		adjacencyMatrix[2][1] = 1;
+        adjacencyMatrix[0][3] = 1;
+        adjacencyMatrix[3][0] = 1;
 
-		adjacencyMatrix[1][4] = 1;
-		adjacencyMatrix[4][1] = 1;
+        adjacencyMatrix[1][2] = 1;
+        adjacencyMatrix[2][1] = 1;
 
-		adjacencyMatrix[1][6] = 1;
-		adjacencyMatrix[6][1] = 1;
+        adjacencyMatrix[1][4] = 1;
+        adjacencyMatrix[4][1] = 1;
 
-		adjacencyMatrix[1][7] = 1;
-		adjacencyMatrix[7][1] = 1;
+        adjacencyMatrix[1][6] = 1;
+        adjacencyMatrix[6][1] = 1;
 
-		adjacencyMatrix[2][3] = 1;
-		adjacencyMatrix[3][2] = 1;
+        adjacencyMatrix[1][7] = 1;
+        adjacencyMatrix[7][1] = 1;
 
-		adjacencyMatrix[2][8] = 1;
-		adjacencyMatrix[8][2] = 1;
+        adjacencyMatrix[2][3] = 1;
+        adjacencyMatrix[3][2] = 1;
 
-		adjacencyMatrix[2][9] = 1;
-		adjacencyMatrix[9][2] = 1;
+        adjacencyMatrix[2][8] = 1;
+        adjacencyMatrix[8][2] = 1;
 
-		adjacencyMatrix[4][5] = 1;
-		adjacencyMatrix[5][4] = 1;
+        adjacencyMatrix[2][9] = 1;
+        adjacencyMatrix[9][2] = 1;
 
-		adjacencyMatrix[4][6] = 1;
-		adjacencyMatrix[6][4] = 1;
+        adjacencyMatrix[4][5] = 1;
+        adjacencyMatrix[5][4] = 1;
 
-		adjacencyMatrix[4][7] = 1;
-		adjacencyMatrix[7][4] = 1;
+        adjacencyMatrix[4][6] = 1;
+        adjacencyMatrix[6][4] = 1;
 
-		// Create a Graph object with the adjacency matrix
-		Graph graph = new Graph(adjacencyMatrix);
+        adjacencyMatrix[4][7] = 1;
+        adjacencyMatrix[7][4] = 1;
 
-		// Define the vertices of the graph
-		Vertex[] nodes = new Vertex[10];
-		for (int i = 0; i < 10; i++) {
-			nodes[i] = new Vertex(i);
-		}
+        // Create a Graph object with the adjacency matrix
+        Graph graph = new Graph(adjacencyMatrix);
 
-		// Print the adjacency matrix to verify the graph
-		graph.printAdjacencyMatrix();
+        // Define the vertices of the graph
+        Vertex[] nodes = new Vertex[n];
+        for (int i = 0; i < n; i++) {
+            nodes[i] = new Vertex(i);
+        }
 
-		// Perform BFS starting from node 0 using the adjacency matrix from the Graph
-		// object
-		Searches.BFS(nodes, graph.getAdjacencyMatrix(), 10, 0);
-	}
+        // Print the adjacency matrix to verify the graph
+        graph.printAdjacencyMatrix();
+
+        // Perform BFS starting from node 0
+        System.out.println("\nPerforming BFS:");
+        Searches.BFS(nodes, graph.getAdjacencyMatrix(), n, 0);
+
+        // Reset visited status for all nodes for DFS
+        for (int i = 0; i < n; i++) {
+            nodes[i].setVisited(false);
+        }
+
+        // Perform DFS starting from node 0
+        System.out.println("\nPerforming DFS:");
+        Searches.DFS(nodes, graph.getAdjacencyMatrix(), n, 0);
+    }
 }
--- breadth-first-search/src/main/java/com/peter_burbery/breadth_first_search/breadth_first_search/Searches.java
+++ breadth-first-search/src/main/java/com/peter_burbery/breadth_first_search/breadth_first_search/Searches.java
@@ -9,6 +9,8 @@ package com.peter_burbery.breadth_first_search.breadth_first_search;
  */
 
 import com.peter_burbery.mu_queue.mu_queue.MUQueue;
+import com.peter_burbery.postfix_stack.postfix_stack.MUStack;
+
 
 public class Searches {
 
@@ -46,6 +48,45 @@ public class Searches {
                 if (adjacency[toVisit][j] == 1 && !nodes[j].getVisited()) {
                     myQueue.enqueue(nodes[j]);
                     nodes[j].setVisited(true);
+                }
+            }
+        }
+    }
+    // Depth-First Search (DFS) method with backtracking
+ // Depth-First Search (DFS) method with backtracking
+    public static void DFS(Vertex[] nodes, int[][] adjacency, int n, int start) {
+        MUStack<Vertex> stack = new MUStack<>();
+        stack.push(nodes[start]);
+        nodes[start].setVisited(true);
+
+        System.out.println("Starting DFS traversal from node: " + start);
+
+        while (!stack.isEmpty()) {
+            Vertex current = stack.peek(); // Look at the top of the stack without removing it yet
+            int toVisit = current.getIndex();
+
+            boolean foundUnvisitedNeighbor = false;
+
+            // Explore neighbors of the current node
+            for (int j = 0; j < n; j++) {
+                if (adjacency[toVisit][j] == 1 && !nodes[j].getVisited()) {
+                    stack.push(nodes[j]); // Push the unvisited neighbor onto the stack
+                    nodes[j].setVisited(true); // Mark the neighbor as visited
+                    System.out.println("Visiting node " + j + " from node " + toVisit);
+                    foundUnvisitedNeighbor = true; // Found a new node to visit
+                    break; // Break the loop and explore deeper
+                }
+            }
+
+            // If no unvisited neighbors were found, we backtrack by popping the stack
+            if (!foundUnvisitedNeighbor) {
+                stack.pop(); // Backtrack by removing the current node
+                if (!stack.isEmpty()) {
+                    // Peek the new top of the stack to know where we're backtracking to
+                    System.out.println("Backtracking to node " + stack.peek().getIndex() + " from node " + toVisit);
+                } else {
+                    // If the stack is empty, we're back at the start
+                    System.out.println("Backtracking to start from node " + toVisit);
                 }
             }
         }
